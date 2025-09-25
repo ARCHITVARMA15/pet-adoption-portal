2ac515fb2dd327a15b36f1071e951a0d45bfddd3
unknown <architvarma25@gmail.com>
2025-09-26 00:55:38 +0530

    Hackathon project - Pet Adoption Portal (login + filters implemented)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import RegistrationForm from './RegistrationForm.jsx'
+ import RegistrationForm from './RegistrationForm.jsx'
 import Leaderboard from "./Leaderboard.jsx";
+//import Login from "./Login.jsx";
 
 function App() {
   return (
     <>
-  <RegistrationForm/>
-    <Leaderboard/>
+      <RegistrationForm />
     </>
   );
 }
